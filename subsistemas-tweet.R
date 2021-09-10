fb22e47f143a4f862b50cacb72b984c84cb7bb15
Bruno Mioto <brunomioto97@gmail.com>
2021-09-10 11:12:36 -0300

    Update subsistemas-tweet.R

--- subsistemas-tweet.R
+++ subsistemas-tweet.R
@@ -1,7 +1,9 @@
 # Install the package from CRAN
-install.packages("rtweet")
-install.packages("devtools")
-devtools::install_github('brunomioto/reservatoriosBR')
+install.packages(.)
+
+# Load packages
+library("rtweet")
+library("reservatoriosBR")
 
 reservatorios_token <- rtweet::create_token(
   app = "reservatoriosBR-bot",
@@ -17,9 +19,9 @@ perc_subsistemas <- reservatoriosBR::ONS_reservatorios(formato = "resumo")
 
 # Build the status message
 tweet_text <- paste0(
-  "Nível dos subsistemas de reservatórios em ", format(Sys.Date(), "%d/%m/%y"), ":\n",
-  perc_subsistemas[1,2],": ", round(perc_subsistemas[1,3],2),"%\n",
+  "Nível dos subsistemas de reservatórios em ", format(Sys.Date(), "%d/%m/%y"), ":\n\n",
   perc_subsistemas[2,2],": ", round(perc_subsistemas[2,3],2),"%\n",
+  perc_subsistemas[1,2],": ", round(perc_subsistemas[1,3],2),"%\n",
   perc_subsistemas[3,2],": ", round(perc_subsistemas[3,3],2),"%\n",
   perc_subsistemas[4,2],": ", round(perc_subsistemas[4,3],2),"%\n\n",
   "Dados da ONS obtidos a partir do pacote reservatoriosBR"
