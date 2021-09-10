8902e33c5e432c796e5d9546609482b69f662b3b
Bruno Mioto <brunomioto97@gmail.com>
2021-09-10 10:02:05 -0300

    Update and rename londonmapbot-tweet.R to subsistemas-tweet.R

--- subsistemas-tweet.R
+++ subsistemas-tweet.R
@@ -17,12 +17,12 @@ perc_subsistemas <- reservatoriosBR::ONS_reservatorios(formato = "resumo")
 
 # Build the status message (text and URL)
 tweet_text <- paste0(
-  "Nível dos subsistemas de reservatórios hoje ", format(Sys.Date(), "%d/%m/%y"), ":\n ",
+  "Nível dos subsistemas de reservatórios em ", format(Sys.Date(), "%d/%m/%y"), ":\n",
   perc_subsistemas[1,2],": ", round(perc_subsistemas[1,3],2),"%\n",
   perc_subsistemas[2,2],": ", round(perc_subsistemas[2,3],2),"%\n",
   perc_subsistemas[3,2],": ", round(perc_subsistemas[3,3],2),"%\n",
   perc_subsistemas[4,2],": ", round(perc_subsistemas[4,3],2),"%\n\n",
-  "Dados obtidos a partir do pacote reservatoriosBR"
+  "Dados da ONS obtidos a partir do pacote reservatoriosBR"
 )
 
 # Post the image to Twitter
